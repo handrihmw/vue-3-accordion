a708646fa5216b3ef0d098467255c6431c57ba1d
Handri Hermawan <37602237+handrihmw@users.noreply.github.com>
2022-08-18 09:23:31 +0700

    Create accordionContent.vue

---
+++ accordionContent.vue
@@ -0,0 +1,13 @@
+<template>
+  <div class="as-accordion__body" v-if="visible()">
+    <div class="as-accordion__content">
+      <slot />
+    </div>
+  </div>
+</template>
+
+<script setup>
+import { inject } from "vue";
+
+const visible = inject("visible");
+</script>
