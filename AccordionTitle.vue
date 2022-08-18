1e79bfa89e0cc625f036ad8f8d6eaa08fd682de2
Handri Hermawan <37602237+handrihmw@users.noreply.github.com>
2022-08-18 09:24:24 +0700

    Create AccordionTitle.vue

---
+++ AccordionTitle.vue
@@ -0,0 +1,23 @@
+<template>
+  <div
+    @click="toggleAccordion"
+    class="as-accordion__header"
+    :class="{ 'as-accordion__header--open': visible() }"
+  >
+    <slot />
+    <i
+      class="as-icon-blue"
+      :class="{
+        'as-icon-arrow-top': visible(),
+        'as-icon-arrow-down': !visible(),
+      }"
+    ></i>
+  </div>
+</template>
+
+<script setup>
+import { inject } from "vue";
+
+const visible = inject("visible");
+const toggleAccordion = inject("toggleAccordion");
+</script>
